e594b2cbc4ada67616bdd3eb77c330fd212f83a5
Matheus <you@example.com>
2021-11-04 12:57:25 -0300

    Completed '2. Display a list' step of the tutorial

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+
+// TODO: click event
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +11,10 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
+  heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
   }
 
   constructor() {}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 20, name: 'Dr. Bad'},
+    {id: 21, name: 'Punisher'},
+    {id: 22, name: 'Groot'},
+    {id: 23, name: 'Dr. Strange'},
+    {id: 24, name: 'Iron Man'},
+    {id: 25, name: 'Chapolin'},
+    {id: 26, name: 'Batman'},
+    {id: 27, name: 'Magma'},
+    {id: 28, name: 'Tornado'},
+    {id: 29, name: 'KIS Hero'},
+    {id: 30, name: 'Staple Man'},
+];
